dff2f95e362ac392f2117bc4571a9b2b4a3af577
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-01-06 11:56:46 -0300

    Initial Commit

---
+++ player/playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  playerApp.swift
+//  player
+//
+//  Created by Jonn Alves on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
